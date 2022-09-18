f7e59306e2b3d1f9b711ffcc68c047b1cfd9a010
danieljesusgonz <danieljesusgonz@gmail.com>
2022-09-18 17:42:43 +0000

    change

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
 /* eslint-disable */
-import "bootstrap";
+//import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pinta = generateRandomSuit();
+
+  if (pinta == "♥" || pinta == "♦") {
+    document.querySelector(".PintaArriba").style.color = "red";
+    document.querySelector(".PintaAbajo").style.color = "red";
+  }
+
+  document.querySelector(".PintaArriba").innerHTML = pinta;
+  document.querySelector(".PintaAbajo").innerHTML = pinta;
+  document.querySelector(".Number").innerHTML = generateRandomNumber();
+};
+
+let generateRandomNumber = () => {
+  let numbers = ["A", "2", "3", "4", "5", "6", "7", "9", "10", "J", "K", "K"];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+let generateRandomSuit = () => {
+  let suit = ["♦", "♠", "♥", "♣"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
 };
